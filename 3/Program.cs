5b8000e7ec60f5176b86f863f16f42f6cb22294a
Natthaya-Polp <natthayacud56@gmail.com>
2023-04-30 23:37:14 +0700

    HW4

---
+++ 3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int nodes = int.Parse(Console.ReadLine());
+        List<int>[] List = new List<int>[nodes];
+
+        for (int i = 0; i < nodes; i++)
+        {
+            List[i] = new List<int>();
+        }
+
+        while (true)
+        {
+            int startNode = int.Parse(Console.ReadLine());
+
+            if (startNode < 0 || startNode >= nodes)
+            {
+                break;
+            }
+
+            int endNode = int.Parse(Console.ReadLine());
+
+            if (endNode < 0 || endNode >= nodes)
+            {
+                break;
+            }
+
+            List[startNode].Add(endNode);
+        }
+
+        int startCheck = int.Parse(Console.ReadLine());
+        int endCheck = int.Parse(Console.ReadLine());
+
+        bool[] visited = new bool[nodes];
+        bool reachable = Check(List, visited, startCheck, endCheck);
+
+        if (reachable)
+        {
+            Console.WriteLine("Reachable");
+        }
+        else
+        {
+            Console.WriteLine("Unreachable");
+        }
+    }
+
+    static bool Check(List<int>[] adjList, bool[] visited, int i, int j)
+    {
+        visited[i] = true;
+
+        if (i == j)
+        {
+            return true;
+        }
+        
+        foreach (int neighbor in adjList[i])
+        {
+            if (!visited[neighbor])
+            {
+                bool reachable = Check(adjList, visited, neighbor, j);
+                if (reachable)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) 
+    {
+        int numNodes;
+        numNodes = int.Parse(Console.ReadLine());
+        bool[,] graph = new bool[numNodes, numNodes];
+        int startNode, endNode;
+
+        while (true) 
+        {
+            startNode = int.Parse(Console.ReadLine());
+
+            if (startNode < 0 || startNode >= numNodes) break;
+
+            endNode = int.Parse(Console.ReadLine());
+
+            if (endNode < 0 || endNode >= numNodes) break;
+
+            if (startNode == endNode) continue;
+
+            graph[startNode, endNode] = true;
+            graph[endNode, startNode] = true;
+        }
+
+        int count = 0;
+        for (int i = 0; i < numNodes; i++)
+        {
+            char symbol = 'A';
+            bool hasEdge = false;
+
+            for (int j = 0; j < numNodes; j++) 
+            {
+                if (graph[i, j]) 
+                {
+                    hasEdge = true;
+                    break;
+                }
+            }
+
+            if (hasEdge) count++;
+            else symbol++;
+        }
+
+        Console.WriteLine(count-2);
+    }
+}
